b9ecc675384dfa6cf5910c80b8e2a780ce8625e3
Pedro Henrique <ph.andradesousa31@gmail.com>
2026-08-27 21:30:25 -0300

    fix: adjusts pnpm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './styles/globals.css';
-import { Toaster } from 'sonner';
 import { Header } from '@/components/header';
+import { Toaster } from 'sonner';
 
 const inter = Inter({
   variable: '--font-inter',
